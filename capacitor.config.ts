9ce62d7a178d4401e47980cffc9900fc5b35ba89
Albert <38384581+Albert-Szczepanski@users.noreply.github.com>
2023-03-19 11:41:40 +0100

    setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'humble-habit-tracker',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
