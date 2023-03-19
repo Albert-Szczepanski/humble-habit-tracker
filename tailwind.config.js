9cc872cf90505e3974d157b5e0e0164b14a149d0
Albert <38384581+Albert-Szczepanski@users.noreply.github.com>
2023-03-19 14:49:01 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{ts,html}'], // Update this line to include Ionic's file extensions
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
